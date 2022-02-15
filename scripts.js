dfe42a4bcf8d0cd2ee77881aeea656d109e06b33
shanemitnick <shane.mitnick21@gmail.com>
2022-02-15 15:59:07 -0500

    just needs sizing changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,41 @@
-console.log("hi");
+let count = 0;
+let visableCircle = true;
+
+function textCircleCLicked() {
+  count += 1;
+  console.log(count);
+  let circleText = document.getElementById("text-circle");
+  circleText.textContent = count;
+}
+
+
+function hoverOverBorderCircle() {
+  let circle = document.getElementById("hover-circle");
+  circle.setAttribute("stroke", "black");
+
+}
+
+function hoverOverBorderCircleOut() {
+  let circle = document.getElementById("hover-circle");
+  circle.setAttribute("stroke", "lightgreen");
+}
+
+
+function disapearCircle() {
+  let circle = document.getElementById("hover-disappear");
+
+
+  if (visableCircle) {
+    circle.style.opacity = "0%";
+    visableCircle = false;
+  } else {
+    circle.style.opacity = "100%";
+    visableCircle = true;
+  }
+
+}
+
+function resetCount() {
+  count = -1;
+  textCircleCLicked();
+}
